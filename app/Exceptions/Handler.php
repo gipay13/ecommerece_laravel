0340bfcf7062266b43e4b88de4caacc8723a17b3
gipay13 <ghifariansar13@gmail.com>
2021-12-16 16:56:13 +0700

    install laravel breeze

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -32,6 +32,16 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
+
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
+
     public function register()
     {
         $this->reportable(function (Throwable $e) {
